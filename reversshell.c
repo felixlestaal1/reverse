c60b3fd8d005250ae038cf2ea79f9c87b91a8250
felixlestaal1 <felixlestaal1@gmail.com>
2023-03-15 19:11:39 -0700

    Disclaimer

--- reversshell.c
+++ reversshell.c
@@ -113,3 +113,11 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrev,LPSTR lpCmdLine,int nC
   MessageBox(NULL,TEXT("Bailaste"),TEXT("Installer"),MB_OK | MB_ICONERROR);
   Shell();
 }
+/*This tool may only be used for educational purposes. The user assumes full responsibility for any 
+action taken with the use of the tool. The author is not responsible for any damage caused by this tool 
+offered on his site. If these terms do not suit your needs, do not use this tool. By using it, you declare 
+to have taken knowledge of the terms mentioned above.
+Esta herramienta solo puede ser utilizada para fines educativos. El usuario asume toda la responsabilidad 
+por cualquier acción tomada con el uso de la herramienta. El autor no se hace responsable por cualquier daño 
+causado por esta herramienta ofrecida en su página. Si estos términos no se ajustan a sus necesidades,no utilice 
+esta herramienta. Al utilizarla, usted declara haber tomado conocimiento de los términos mencionados anteriormente.*/
